27d65f7a37fd64ec2fe215591641f3d9265e94db
Roch Blondiaux <roch.blondiaux@protonmail.com>
2026-01-07 16:49:28 +0100

    init: project setup

---
+++ src/main/java/com/astralrealms/classes/AstralClasses.java
@@ -0,0 +1,25 @@
+package com.astralrealms.classes;
+
+import com.astralrealms.core.paper.plugin.AstralPaperPlugin;
+
+public final class AstralClasses extends AstralPaperPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+
+        // Configuration
+        this.loadConfiguration();
+
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void loadConfiguration() {
+
+    }
+}
