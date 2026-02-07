849d9b7c70a6eec770ed13675dadcc2f37e28b1b
Roch Blondiaux <roch.blondiaux@protonmail.com>
2026-02-07 17:51:36 +0100

    feat: freeze hud on stat modifier

--- src/main/java/com/astralrealms/classes/skill/StatModifierSkill.java
+++ src/main/java/com/astralrealms/classes/skill/StatModifierSkill.java
@@ -1,6 +1,7 @@
 package com.astralrealms.classes.skill;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -20,6 +21,10 @@ import com.astralrealms.stats.StatsAPI;
 import com.astralrealms.stats.model.modifier.ModifierSource;
 import com.astralrealms.stats.model.modifier.ModifierType;
 import com.astralrealms.stats.model.modifier.impl.EnumBasedStatModifier;
+import com.github.retrooper.packetevents.PacketEvents;
+import com.github.retrooper.packetevents.protocol.entity.data.EntityData;
+import com.github.retrooper.packetevents.protocol.entity.data.EntityDataTypes;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerEntityMetadata;
 
 import net.kyori.adventure.key.Key;
 
@@ -43,18 +48,47 @@ public record StatModifierSkill(Duration cooldown, Duration duration, Key key, K
             if (state.expiresAt() > now)
                 continue;
 
+            // Remove stat modifier
             StatsAPI.removeModifier(player, key);
+
+            // Remove from cache
             states.remove(entry.getKey());
+
+            // Send metadata update
+            WrapperPlayServerEntityMetadata metadataPacket = new WrapperPlayServerEntityMetadata(
+                    player.getEntityId(),
+                    List.of(
+                            new EntityData<>(7, EntityDataTypes.INT, 0)
+                    )
+            );
+            PacketEvents.getAPI().getPlayerManager().sendPacket(player, metadataPacket);
         }
     }
 
     @Override
     public void trigger(Player player, InputType inputType, @Nullable SkillContext context) {
+        if (states.cache().containsKey(player.getUniqueId()))
+            return;
+
+        // Add stat modifier
         StatsAPI.addModifier(player, stat, new EnumBasedStatModifier<>(key, stat, ModifierSource.OTHER, EquipmentSlot.SADDLE, value, modifier, input));
+
+        // Cache the state
         states.edit(
                 player.getUniqueId(),
                 StatModifierState::new,
                 state -> state.expiresAt(System.currentTimeMillis() + duration.toMillis())
         );
+
+        // Send metadata update
+        WrapperPlayServerEntityMetadata metadataPacket = new WrapperPlayServerEntityMetadata(
+                player.getEntityId(),
+                List.of(
+                        new EntityData<>(7, EntityDataTypes.INT, 140)
+                )
+        );
+        PacketEvents.getAPI().getPlayerManager().sendPacket(player, metadataPacket);
+
+
     }
 }
